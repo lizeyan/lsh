1b42eef792b09234e54bda31f5bc0edac1e8f21c
zoesgithub <xcc14@mails.tsinghua.edu.cn>
2019-05-28 14:28:21 +0800

    add LSHFOREST

---
+++ LSH_forest/lsh_forest.py
@@ -0,0 +1,237 @@
+import sys
+sys.path.append("..")
+#from lsh import BasicE2LSH
+import numpy as np
+from loguru import logger
+#import pygtries as tries
+from lsh.hash_family import E2Family
+import random
+import networkx as nx
+
+class node(object):
+    def __init__(self,obj, father, lchild=None, rchild=None, isleaf=False):
+        self.father=father
+        self.obj=obj
+        self.lchild=lchild
+        self.rchild=rchild
+        self.isleaf=isleaf
+
+    def up(self):
+        return self.father
+
+    def query(self):
+        return self.obj
+
+    def ldown(self, create=False):
+        if self.isleaf:
+            raise ValueError("Leaf node do not have left child")
+        new=False
+        if create: 
+            if self.lchild is None:
+                self.lchild=node('0', self)
+                new=True
+            elif self.lchild.isleaf:
+                obj=self.lchild.obj
+                p=obj[0][obj[-1]]
+                obj[-1]+=1
+                if p=='0':
+                    newnode=node('0', self, self.lchild)
+                    self.lchild.father=newnode
+                    self.lchild=newnode
+                else:
+                    newnode=node('1', self, self.lchild)
+                    self.lchild.father=newnode
+                    self.rchild=newnode
+                    self.lchild=node('0', self)
+                    new=True
+                
+        return self.lchild, new
+
+    def rdown(self, create=False):
+        if self.isleaf:
+            raise ValueError('Leaf node do not have right child')
+        new=False
+        if create:
+            if self.rchild is None:
+                self.rchild=node('1', self)
+                new=True
+        return self.rchild, new
+
+    def add_lchild(self, Node):
+        if self.isleaf:
+            raise ValueError("Leaf node cannot add child")
+        self.lchild=Node
+
+    def add_rchild(self, Node):
+        if self.isleaf:
+            raise ValueError('Leaf node cannnot add child')
+        self.rchild=Node
+
+class Btree(object):
+    def __init__(self):
+        self.root=node(None, None)
+
+    def insert(self, hashcode, term):
+        Node=self.root
+        depth=0
+        for t in hashcode:
+            depth+=1
+            Node, isnew=(Node.ldown(True) if t=='0' else Node.rdown(True))
+            if isnew:
+                break
+        if Node.lchild is None:
+            Node.add_lchild(node([hashcode, [term], depth], Node, isleaf=True))
+        else:
+            #print (Node.lchild.isleaf)
+            if len(Node.lchild.obj[1])>100:
+                logger.debug(f"The leaf has existed and the number if {len(Node.lchild.obj[1])}")
+            Node.lchild.obj[1].append(term)
+
+    def delete(self, hashcode, term):
+        depth=0
+        Node=self.root
+        for t in hashcode:
+            depth+=1
+            Node, _=( Node.ldown() if t=='0' else Node.rdown())
+            if Node.lchild.isleaf:
+                obj=Node.lchild.obj
+                if len(obj[1])>1:
+                    for index in range(len(obj[1])):
+                        value=(np.equal(term, obj[1][index])).sum()
+                        if int(value)==len(term):
+                            del obj[1][index]
+                            return
+                    raise ValueError("The queried item not exist in this tree")
+                value=(np.equal(term, obj[1][0])).sum()
+                if int(value)==len(term):
+                    Node.lchild=None
+                    depth-=1
+                    while depth>-1:
+                        Node=Node.up()
+                        t=hashcode[depth]
+                        if t=='0':
+                            if not( Node.rchild is None):
+                                Node.lchild=None
+                                return
+                        else:
+                            if not(Node.lchild is None):
+                                Node.rchild=None
+                                return
+                        depth-=1
+                    t=hashcode[0]
+                    if t=='0':
+                        Node.lchild=None
+                        return
+                    if t=='1':
+                        Node.rchild=None
+                        return
+                else:
+                    raise ValueError("The queried item not exist in this tree")
+
+    def descend(self, hashcode):
+        def desc(q, Node, depth):
+            if Node.lchild.isleaf:
+                return (Node.lchild, depth)
+            t=q[depth]
+            depth+=1
+            if t=='0':
+                newNode=Node.lchild
+            elif t=='1':
+                newNode=Node.rchild
+            if newNode is None:
+                return (Node, depth-1)
+            Node=newNode
+            return desc(q, Node, depth)
+        Node, depth=desc(hashcode, self.root, 0)
+        return (Node, depth)
+
+    def descendants(self, Node, res):
+        def descen(Node, res):
+            if Node is None:
+                return
+            if not(Node.lchild is None) and Node.lchild.isleaf:
+                res.add(Node.lchild)
+                return
+            descen(Node.lchild, res)
+            descen(Node.rchild, res)
+        descen(Node, res)
+
+class LSHTree(object):
+    def __init__(self, km):
+        self.km=km
+        self.generator=E2Family(50, k=km)
+        self.func=self.generate_hashfunc()
+        self.tree=Btree()
+
+    def query(self, term):
+        hashcode=self.list2vec(term)
+        return self.tree.descent(hashcode)
+
+    def add_batch(self, q_list):
+        vec=[self.list2vec(item) for item in q_list]
+        for item, index in zip(vec, range(len(vec))):
+            self._add_one(item, q_list[index])
+
+    def add_one(self, image):
+        hashcode=self.list2vec(image)
+        self._add_one(hashcode, image)
+
+    def delete_one(self, image):
+        hashcode=self.list2vec(image)
+        self.tree.delete(hashcode, image)
+
+    def _add_one(self, hashcode, term):
+        self.tree.insert(hashcode, term)
+
+    def list2vec(self, term):
+        code=np.array(self.func(term))
+        code=code%2
+        code=code.tolist()
+        code=[str(item) for item in code]
+        return ''.join(code)
+
+    def generate_hashfunc(self):
+        return self.generator.sample()
+
+    def descend(self, image):
+        hashcode=self.list2vec(image)
+        return self.tree.descend(hashcode)
+
+    def descendants(self, Node, res):
+        return self.tree.descendants(Node, res)
+        
+
+class LSHForest(object):
+    def __init__(self, l, km):
+        self.km=km ##the maximum length
+        self.l=l ## the number of trees
+        self.forest=[LSHTree(self.km) for _ in range(self.l)]
+
+    def add_batch(self, q_list):   #build hash table
+        [tree.add_batch(q_list) for tree in self.forest]
+
+    def add_one(self, image):
+        [tree.add_one(image) for tree in self.forest]
+
+    def delete_one(self, image):
+        [tree.delete_one(image) for tree in self.forest]
+
+
+    def query(self, q, c=2, m=1):  # query from hash table
+        descend=[tree.descend(q) for tree in self.forest]
+        res=set([])
+
+        x=max([item[1] for item in descend])
+        while (x>0 and (len(res)<c*self.l or len(res)<m)):
+            for i in range(self.l):
+                if descend[i][1]==x:
+                    self.forest[i].descendants(descend[i][0], res)
+                    descend[i]=[descend[i][0].up(), descend[i][1]-1]
+            x-=1
+        res=list(res)
+        res_=[]
+        for Node in res:
+            res_.extend(Node.obj[1])
+        logger.info(f"FINISH QUERY, the number of candidate is {len(res_)}")
+        return res_
+
---
+++ run_lshforest_evaluation.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+import click
+import numpy as np
+from loguru import logger
+
+from config import n_dims, n_test_samples, n_train_samples
+from lsh import LSH, BasicE2LSH
+from lsh.multi_probe_lsh import MultiProbeE2LSH
+from LSH_forest.lsh_forest import LSHForest
+
+data_base_path = Path("./outputs")
+train_data = np.memmap(data_base_path / 'train_arr', mode='r', dtype=np.float32, shape=(n_train_samples, n_dims))
+test_data = np.memmap(data_base_path / 'test_arr', mode='r', dtype=np.float32, shape=(n_test_samples, n_dims))
+ground_truth = np.memmap(
+    data_base_path / 'ground_truth', mode='r', dtype=np.int32, shape=(n_test_samples, n_train_samples)
+)
+
+
+def e2_ratio(q, labels, preds):
+    """
+    :param q: the query vector in shape (d,)
+    :param labels: label, in shape (k, d)
+    :param preds: predict, in shape (k, d)
+    :return:
+    """
+    e2 = lambda x, y: np.sqrt(np.sum((x - y) ** 2, axis=-1))
+    q = np.expand_dims(q.copy(), 0)
+    return np.mean(e2(q, preds) / e2(q, labels))
+
+
+def lsh_evaluation(lsh: LSH, **kwargs):
+    handler_id = logger.add(data_base_path / 'logs' / f'{lsh}.log'.replace(' ', '_'))
+    logger.info(lsh)
+    logger.info(f"start add entries, train data shape: {np.shape(train_data)}")
+    lsh.add_batch(train_data[:60000])
+    logger.info(f"start evaluate")
+    error_ratio_list = []
+    for idx, test_sample in enumerate(test_data):
+        candidates = lsh.query(test_sample, **kwargs)
+        n_candidates = np.size(candidates, 0)
+        # TODO ?
+        if n_candidates <= 0:
+            continue
+        labels = train_data[ground_truth[idx, :n_candidates]]
+        error_ratio_list.append(e2_ratio(test_sample, labels, candidates))
+    logger.info(f"error ratio: {np.mean(error_ratio_list)}")
+    logger.remove(handler_id)
+
+
+@click.command()
+def main():
+    # lsh_evaluation(BasicE2LSH(n_dims=50, n_hash_table=44, n_compounds=10, w=10.))
+    #lsh_evaluation(MultiProbeE2LSH(n_dims=50, n_hash_table=44, n_compounds=10, w=10.), t=1)
+    lsh_evaluation(LSHForest(l=10, km=30))
+
+
+if __name__ == '__main__':
+    main()
